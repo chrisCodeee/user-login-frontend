9487e0027c6affad93bed60f85dd4b03658fc150
Chris <nzeanoruec@gmail.com>
2024-07-08 13:41:22 -0400

    modified the dist folder

--- src/route.tsx
+++ src/route.tsx
@@ -3,6 +3,11 @@ import { Dashboard, Welcome } from "./pages";
 import { Login, Register } from "./auth";
 import Error from "./pages/Error";
 
-const route = createBrowserRouter([{ errorElement: <Error /> }, { path: "/", element: <Welcome /> }, { path: "/users/login", element: <Login /> }, { path: "/users/register", element: <Register /> }, { path: "/dashboard", element: <Dashboard /> }]);
+const route = createBrowserRouter([
+	{ path: "/", element: <Welcome />, errorElement: <Error /> },
+	{ path: "/users/login", element: <Login /> },
+	{ path: "/users/register", element: <Register /> },
+	{ path: "/dashboard", element: <Dashboard /> },
+]);
 
 export default route;
